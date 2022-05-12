167f9f3bfd978e609e36047760074edead85e4dc
Daniel Alfredo Rodriguez Mendoza <daniel.mendoza@mvm.com.co>
2022-05-12 01:44:26 -0500

    Cambio 12/05/2022 1

--- index.php
+++ index.php
@@ -131,3 +131,10 @@
 <?php
 	require("pie.php");
 ?>
+<footer class='w3-display-bottommiddle' style='text-align:center;'>
+  		<p>
+			<img src="imagenes/vinkasoftware.png" width="50px" class="logo-inicial">
+			<br>
+			Powered by VinkaSoftware
+		</p>
+	</footer> 
